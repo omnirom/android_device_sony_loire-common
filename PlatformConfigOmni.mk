a96c4d199ed7ab039f3508a70671aa8fb0c1dbe0
Erik Castricum <git@cas-online.nl>
2017-02-05 21:35:37 +0100

    loire: fix incall BT
    needs android_hardware_qcom_audio updates
    
    Change-Id: I4329daac944fe63c94dfbac18ee4f026e05a0535
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- PlatformConfigOmni.mk
+++ PlatformConfigOmni.mk
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+AUDIO_FEATURE_ENABLED_SOURCE_TRACKING := true
+
 include device/sony/loire-common/PlatformConfig.mk
