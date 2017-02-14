38fc4fa5a0a6b8a70b3419d91c73ad92cceee525
Humberto Borba <humberos@gmail.com>
2017-02-14 16:25:06 -0200

    sony: loire: Add SnapdragonCamera
    Change-Id: Ied64689b913c4868b8055a0af6ce21f96dee69b8
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- platform_omni.mk
+++ platform_omni.mk
@@ -17,6 +17,10 @@ SONY_ROOT = device/sony/loire-common/rootdir
 # Common config
 include device/sony/loire-common/platform.mk
 
+# Snapdragon Camera
+PRODUCT_PACKAGES += \
+    SnapdragonCamera
+
 # Copying files
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/twrp.fstab:recovery/root/etc/twrp.fstab
