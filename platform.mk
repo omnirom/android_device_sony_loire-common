bdf8c66966cb2b5a25fecf46d7e982ac8c67ee6f
Erik Castricum <git@cas-online.nl>
2017-02-24 21:24:24 +0100

    camera: condition AOSP and CAF
    Signed-off-by: Erik Castricum <git@cas-online.nl>

--- platform.mk
+++ platform.mk
@@ -86,8 +86,18 @@ PRODUCT_PACKAGES += \
     gps.msm8952
 
 # CAMERA
+ifneq (,$(filter true, $(BOARD_USES_QCOM_HARDWARE)))
+# CAMERA
 PRODUCT_PACKAGES += \
     camera.msm8952
+else
+# CAMERA & SENSORS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.qc.sdk.sensors.gestures=true \
+    ro.qc.sdk.gestures.camera=false \
+    ro.qc.sdk.camera.facialproc=false \
+    camera.disable_zsl_mode=1
+endif
 
 # Keymaster
 PRODUCT_PACKAGES += \
