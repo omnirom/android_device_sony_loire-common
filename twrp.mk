ab970952d337abec262c50f41bb9ed29e33da9cd
Humberto Borba <humberos@gmail.com>
2017-04-15 10:41:10 -0300

    sony: loire: twrp: Add missed qseecomd deps for encrypted devices
    1. Use the same stock fstab format except for data partition
    2. Add script to copy required libs for qseecomd on twrp recovery
    3. Update init by setting crypto framework properties
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I0cab949224ab0f43b4287cc217a29c5c11bccce2

--- twrp.mk
+++ twrp.mk
@@ -2,6 +2,17 @@
 # This is for TWRP Recovery
 #
 
+COMMON_PATH := device/sony/loire-common
+VENDOR_PATH := vendor/sony/loire-$(TARGET_DEVICE)/proprietary
+TWRP_OUT := recovery/root
+
 TW_THEME := portrait_hdpi
 TW_MAX_BRIGHTNESS := 4095
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/rootdir/pulldecryptfiles.sh:$(TWRP_OUT)/sbin/pulldecryptfiles.sh \
+    $(VENDOR_PATH)/bin/qseecomd:$(TWRP_OUT)/sbin/qseecomd
+
+PRODUCT_COPY_FILES += \
+    bionic/libc/zoneinfo/tzdata:$(TWRP_OUT)/system/usr/share/zoneinfo/tzdata
