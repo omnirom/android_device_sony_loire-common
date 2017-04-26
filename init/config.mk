42138a244123f7b8ab75a37772928ca6a77d07b3
Humberto Borba <humberos@gmail.com>
2017-04-26 16:58:59 -0300

    sony: loire: Track CAF
    Change-Id: I97bed3a92ad3bde76fde705d9ff0600466375220
    Signed-off-by: Humberto Borba <humberos@gmail.com>

---
+++ init/config.mk
@@ -0,0 +1,36 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Init configuration for init_sony
+BOARD_SONY_INIT_FLAGS :=
+BOARD_SONY_INIT_INCLUDE := $(COMMON_PATH)/include
+
+ifneq ($(filter kugo,$(TARGET_DEVICE)),)
+BOARD_SONY_INIT_FLAGS += -DDEV_BLOCK_FOTA_NUM="46"
+BOARD_SONY_INIT_FLAGS += -DDEV_BLOCK_FOTA_MAJOR="259"
+BOARD_SONY_INIT_FLAGS += -DDEV_BLOCK_FOTA_MINOR="14"
+else
+BOARD_SONY_INIT_FLAGS += -DDEV_BLOCK_FOTA_NUM="42"
+BOARD_SONY_INIT_FLAGS += -DDEV_BLOCK_FOTA_MAJOR="259"
+BOARD_SONY_INIT_FLAGS += -DDEV_BLOCK_FOTA_MINOR="10"
+endif
+
+# FOTA check is broken on all devices
+BOARD_SONY_INIT_FLAGS += -DFOTA_RAMDISK_CHECK="0"
+
+# Ensure keycheck is used on all devices on AOSP
+BOARD_SONY_INIT_FLAGS += -DKEYCHECK_ENABLED="1"
+
+# Only use Volume+ on all devices on AOSP
+BOARD_SONY_INIT_FLAGS += -DKEYCHECK_NO_DOWN="1"
