8ff76fe3a32886463a0c8b3b2d7c069915e20a97
Humberto Borba <humberos@gmail.com>
2017-04-26 16:58:59 -0300

    sony: loire: Track CAF
    Change-Id: I97bed3a92ad3bde76fde705d9ff0600466375220
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -13,9 +13,39 @@
 # limitations under the License.
 
 # Platform path
-PLATFORM_COMMON_PATH := device/sony/loire-common
+COMMON_PATH := device/sony/loire-common
+
+# Headers path
+TARGET_SPECIFIC_HEADER_PATH := $(COMMON_PATH)/include
+
+# Override the default android_filesystem_config.h
+TARGET_ANDROID_FILESYSTEM_CONFIG_H := \
+    $(COMMON_PATH)/include/private/android_filesystem_config.h
+
+# Also use headers from kernel during build process
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+
+# CAF
+TARGET_BOARD_AUTO := true
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_QCOM_DISPLAY_VARIANT := caf-msm8952
+TARGET_QCOM_MEDIA_VARIANT := caf-msm8952
+TARGET_QCOM_AUDIO_VARIANT := caf-msm8952
+
+# CNE
+BOARD_USES_QCNE := true
+
+# We use stock camera blobs
+USE_CAMERA_STUB := true
+
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RECOVERY := false
+TARGET_NO_KERNEL := false
 
 TARGET_BOARD_PLATFORM := msm8952
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno510
+BOARD_USES_ADRENO := true
 
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -34,6 +64,10 @@ TARGET_USES_64_BIT_BCMDHD := true
 
 ENABLE_CPUSETS := true
 
+# kernel
+TARGET_KERNEL_SOURCE := kernel/sony/msm8952
+
+BUILD_KERNEL             := true
 BOARD_KERNEL_BASE        := 0x20000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
@@ -42,6 +76,11 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_CMDLINE += console=ttyHSL0,115200,n8
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += androidboot.bootdevice=7824900.sdhci
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE += dwc3.maximum_speed=high dwc3_msm.prop_chg_detect=Y
+BOARD_KERNEL_CMDLINE += coherent_pool=8M
+BOARD_KERNEL_CMDLINE += sched_enable_power_aware=1 user_debug=31
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
@@ -50,13 +89,43 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# GFX
+USE_OPENGL_RENDERER := true
+TARGET_USES_ION := true
+TARGET_USES_C2D_COMPOSITION := true
+
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+MAX_EGL_CACHE_SIZE := 2048*1024
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+
+# GPS definitions for Qualcomm solution
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
+TARGET_NO_RPC := true
+
+# Charger
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+HEALTHD_FORCE_BACKLIGHT_CONTROL := true
+HEALTHD_ENABLE_TRICOLOR_LED := true
+RED_LED_PATH := /sys/class/leds/led:rgb_red/brightness
+GREEN_LED_PATH := /sys/class/leds/led:rgb_green/brightness
+BLUE_LED_PATH := /sys/class/leds/led:rgb_blue/brightness
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
+
 #Reserve space for data encryption (22225616896-16384)
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 6197084160
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 22225600512
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
-TARGET_RECOVERY_FSTAB = $(PLATFORM_COMMON_PATH)/rootdir/fstab.loire
+TARGET_RECOVERY_FSTAB = $(COMMON_PATH)/rootdir/fstab.loire
 
 # Wi-Fi definitions for Broadcom solution
 BOARD_WLAN_DEVICE           := bcmdhd
@@ -70,10 +139,13 @@ WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
 
 # BT definitions for Broadcom solution
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_COMMON_PATH)/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(COMMON_PATH)/bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_CUSTOM_BT_CONFIG := $(PLATFORM_COMMON_PATH)/bluetooth/vnd_generic.txt
+BOARD_CUSTOM_BT_CONFIG := $(COMMON_PATH)/bluetooth/vnd_generic.txt
+
+# Fixup SELinux file labels
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 
 # RIL
 TARGET_PER_MGR_ENABLED := true
@@ -83,12 +155,74 @@ NXP_CHIP_TYPE := PN547C2
 
 # FM definitions for Broadcom solution
 BOARD_HAVE_ALTERNATE_FM := true
+BOARD_HAVE_FM_RADIO := true
 BOARD_HAVE_BCM_FM := true
+BOARD_DISABLE_FMRADIO_LIBJNI := true
 
-# Props for hotplugging
-TARGET_SYSTEM_PROP += $(PLATFORM_COMMON_PATH)/system.prop
+# Audio
+AUDIO_FEATURE_ENABLED_AAC_ADTS_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_ACDB_LICENSE := true
+AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
+AUDIO_FEATURE_ENABLED_APE_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_AUDIOSPHERE := true
+AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
+AUDIO_FEATURE_ENABLED_CUSTOMSTEREO := true
+AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER := true
+AUDIO_FEATURE_ENABLED_EXTN_FORMATS := true
+AUDIO_FEATURE_ENABLED_FLAC_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_FLUENCE := true
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+AUDIO_FEATURE_ENABLED_HDMI_EDID := true
+AUDIO_FEATURE_ENABLED_HDMI_SPK := true
+AUDIO_FEATURE_ENABLED_HFP := true
+AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+AUDIO_FEATURE_ENABLED_NT_PAUSE_TIMEOUT := true
+AUDIO_FEATURE_ENABLED_PCM_OFFLOAD_24 := true
+AUDIO_FEATURE_ENABLED_PCM_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
+AUDIO_FEATURE_ENABLED_SOURCE_TRACKING := true
+AUDIO_FEATURE_ENABLED_SPKR_PROTECTION := true
+AUDIO_FEATURE_ENABLED_VBAT_MONITOR := true
+AUDIO_FEATURE_ENABLED_VOICE_CONCURRENCY :=true
+AUDIO_FEATURE_ENABLED_VORBIS_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_WMA_OFFLOAD := true
+AUDIO_USE_LL_AS_PRIMARY_OUTPUT := true
+BOARD_USES_ALSA_AUDIO := true
+FIPS_ENABLED := true
+MM_AUDIO_ENABLED_FTM := true
+MM_AUDIO_ENABLED_SAFX := true
+TARGET_USES_QCOM_MM_AUDIO := true
+USE_CUSTOM_AUDIO_POLICY := 1
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
+
+# Multihal
+USE_SENSOR_MULTI_HAL := true
+
+# Camera
+TARGET_USES_MEDIA_EXTENSIONS := true
+
+# Do not build keymaster
+TARGET_PROVIDES_KEYMASTER := true
+
+# Cryptfs hw
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := $(COMMON_PATH)/cryptfs_hw
+
+# Set seccomp policy for media server
+BOARD_SECCOMP_POLICY += $(COMMON_PATH)/seccomp
 
 # SELinux
-BOARD_SEPOLICY_DIRS += $(PLATFORM_COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
-include device/sony/common/CommonConfigOmni.mk
+# Properties
+TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
--- platform.mk
+++ platform.mk
@@ -13,15 +13,15 @@
 # limitations under the License.
 
 # Platform path
-PLATFORM_COMMON_PATH := device/sony/loire-common
+COMMON_PATH := device/sony/loire-common
 
-$(call inherit-product, device/sony/common/common_omni.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
-SOMC_PLATFORM := loire
+SOMC_PLATFORM    := loire
+PRODUCT_PLATFORM := loire
 
-SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
+SONY_ROOT := $(COMMON_PATH)/rootdir
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -30,6 +30,20 @@ PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
     $(SONY_ROOT)/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(SONY_ROOT)/system/etc/media_profiles.xml:system/etc/media_profiles.xml
+
+# Common etc
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/system/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(SONY_ROOT)/system/etc/gps.conf:system/etc/gps.conf \
+    $(SONY_ROOT)/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
+    $(SONY_ROOT)/system/etc/sec_config:system/etc/sec_config \
+    $(SONY_ROOT)/system/etc/sensors/sensors_settings:system/etc/sensors/sensors_settings
+
+# QMI
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/system/etc/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(SONY_ROOT)/system/etc/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(SONY_ROOT)/system/etc/data/qmi_config.xml:system/etc/data/qmi_config.xml
 
 # Broadcom BT
 PRODUCT_COPY_FILES += \
@@ -47,6 +61,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/etc/msm_irqbalance.conf:system/etc/msm_irqbalance.conf
 
+# Variant linking script
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/releasetools/updater.sh:utilities/updater.sh
+
 # Device Specific Hardware
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml \
@@ -55,14 +73,61 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
 
+# Audio
+PRODUCT_COPY_FILES += \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
+
+# Common Specific Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:system/etc/permissions/android.hardware.camera.raw.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:system/etc/permissions/android.hardware.sensor.stepcounter.xml \
+    frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:system/etc/permissions/android.hardware.sensor.stepdetector.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(PLATFORM_COMMON_PATH)/overlay
+    $(COMMON_PATH)/overlay
 
 # Platform Init
 PRODUCT_PACKAGES += \
     fstab.loire \
+    init.loire.usb \
     init.loire.pwr
+
+# Sensor multihal
+PRODUCT_PACKAGES += \
+    sensors.msm8952
+
+# Cryptfs
+PRODUCT_PACKAGES += \
+    libcryptfs_hw
 
 # NFC packages
 PRODUCT_PACKAGES += \
@@ -72,7 +137,27 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.primary.msm8952
+    audiod \
+    audio.a2dp.default \
+    audio.primary.msm8952 \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    libaudioroute \
+    libqcompostprocbundle \
+    libqcomvisualizer \
+    libqcomvoiceprocessing \
+    libqcomvoiceprocessingdescriptors \
+    libqcompostprocbundle \
+    libtinycompress \
+    libtinyalsa \
+    tinymix
+
+# GFX
+PRODUCT_PACKAGES += \
+    libhdmi \
+    libqdutils \
+    libqdMetaData
 
 # GFX
 PRODUCT_PACKAGES += \
@@ -85,13 +170,82 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8952
 
+# OMX
+PRODUCT_PACKAGES += \
+    libc2dcolorconvert \
+    libOmxAacEnc \
+    libOmxAmrEnc \
+    libOmxCore \
+    libOmxEvrcEnc \
+    libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
+    libstagefrighthw
+
+# GPS
+PRODUCT_PACKAGES += \
+    libloc_api_v02 \
+    libloc_core \
+    libloc_eng \
+    libgps.utils
+
+# WLAN
+PRODUCT_PACKAGES += \
+    p2p_supplicant.conf \
+    hostapd \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# OSS
+PRODUCT_PACKAGES += \
+    timekeep \
+    TimeKeep \
+    thermanager \
+    macaddrsetup
+
+# QCOM OSS
+PRODUCT_PACKAGES += \
+    librmnetctl
+
+# QCOM GPS
+PRODUCT_PACKAGES += \
+    libloc_api_v02 \
+    libloc_ds_api
+
+# Charger
+PRODUCT_PACKAGES += \
+    omni_charger_res_images
+
+# AOSP Packages
+PRODUCT_PACKAGES += \
+    Launcher3
+
+PRODUCT_PACKAGES += \
+    libion \
+    libxml2
+
+PRODUCT_PACKAGES += \
+    libemoji
+
+# RIL
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full
+
 # CAMERA
 PRODUCT_PACKAGES += \
-    camera.msm8952
+    libmmcamera_interface \
+    libmmjpeg_interface \
+    libmm-qcamera \
+    libqomx_core
 
-# Keymaster
+# lib camera shim
 PRODUCT_PACKAGES += \
-    keystore.msm8952
+    libshim_wvm
+
+# Snapdragon Camera
+PRODUCT_PACKAGES += \
+    SnapdragonCamera
 
 # FM
 PRODUCT_PACKAGES += \
@@ -99,49 +253,27 @@ PRODUCT_PACKAGES += \
     brcm-uim-sysfs \
     libfmjni
 
-# RILD
-PRODUCT_PROPERTY_OVERRIDES += \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM
+# APN list
+PRODUCT_COPY_FILES += \
+    device/sample/etc/old-apns-conf.xml:system/etc/old-apns-conf.xml \
+    device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
 
-# OpenGLES Nougat version
+# Limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196610
+    dalvik.vm.dex2oat-threads=2 \
+    dalvik.vm.image-dex2oat-threads=4
 
-# aDSP sensors
-## max rate
+# configure adb over wifi only on the eng build
+ifneq (,$(filter eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qti.sensors.max_accel_rate=false \
-    ro.qti.sensors.max_gyro_rate=false \
-    ro.qti.sensors.max_mag_rate=false \
-    ro.qti.sensors.max_geomag_rotv=50
+    service.adb.tcp.port=5555
+endif
 
-## sensor type
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qti.sdk.sensors.gestures=false \
-    ro.qti.sensors.pedometer=false \
-    ro.qti.sensors.step_detector=false \
-    ro.qti.sensors.step_counter=false \
-    ro.qti.sensors.pam=false \
-    ro.qti.sensors.scrn_ortn=false \
-    ro.qti.sensors.smd=false \
-    ro.qti.sensors.game_rv=false \
-    ro.qti.sensors.georv=false \
-    ro.qti.sensors.cmc=false \
-    ro.qti.sensors.bte=false \
-    ro.qti.sensors.fns=false \
-    ro.qti.sensors.qmd=false \
-    ro.qti.sensors.amd=false \
-    ro.qti.sensors.rmd=false \
-    ro.qti.sensors.vmd=false \
-    ro.qti.sensors.gtap=false \
-    ro.qti.sensors.tap=false \
-    ro.qti.sensors.facing=false \
-    ro.qti.sensors.tilt=false \
-    ro.qti.sensors.tilt_detector=false \
-    ro.qti.sensors.dpc=false \
-    ro.qti.sensors.wu=true
+# Init configuration for init_sony
+include $(COMMON_PATH)/init/config.mk
 
-## 8MP Switch for ES
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.camera.8mp.config=true
+# TWRP
+$(call inherit-product, device/sony/loire-common/twrp.mk)
+
+# Omni custom config
+$(call inherit-product, vendor/omni/config/common.mk)
