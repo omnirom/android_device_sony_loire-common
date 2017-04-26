b4fe8e4253db3f2d2de63d274fe7fb8d6647b9d9
Humberto Borba <humberos@gmail.com>
2017-04-26 16:58:59 -0300

    sony: loire: Track CAF
    Change-Id: I97bed3a92ad3bde76fde705d9ff0600466375220
    Signed-off-by: Humberto Borba <humberos@gmail.com>

---
+++ brcm_fmradio/brcm-uim-sysfs/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+#
+# UIM Application
+#
+
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)/include
+
+LOCAL_SRC_FILES:= \
+    uim.c \
+    upio.c \
+    brcm_hci_dump.c \
+    btsnoop.c \
+    utils.c
+
+LOCAL_CLANG := false
+LOCAL_CFLAGS:= -c -W -Wall -O2 -D_POSIX_SOURCE -DUIM_DEBUG -DBLUEDROID_ENABLE_V4L2
+LOCAL_SHARED_LIBRARIES:= libnetutils libcutils liblog
+
+SYSFS_PREFIX := "/sys/bus/platform/drivers/bcm_ldisc/bcm_ldisc/"
+ifneq ($(BOARD_HAVE_BCM_FM_SYSFS),)
+SYSFS_PREFIX := $(BOARD_HAVE_BCM_FM_SYSFS)
+endif
+LOCAL_CFLAGS += -DSYSFS_PREFIX=\"$(SYSFS_PREFIX)\"
+
+LOCAL_MODULE := brcm-uim-sysfs
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
