7c9fc794ccbf5e3ee5855e11ae1c637bc53febdf
Humberto Borba <humberos@gmail.com>
2017-04-30 23:29:36 -0300

    sony: loire: Add missed permissions
    Change-Id: I7b64a5336999ab6f86ed9826cbe4a532beac2143
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- platform.mk
+++ platform.mk
@@ -53,7 +53,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.audio.pro.xml:system/etc/permissions/android.hardware.audio.pro.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
