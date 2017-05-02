2869140b35f1bbff31410bb7f34c57c75a52e1ca
Humberto Borba <humberos@gmail.com>
2017-05-02 15:16:15 -0300

    sony: loire: Clean up environment
    Change-Id: I9b472ab9ad830d46381dcc678515ee6ce0d19b22
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- twrp.mk
+++ twrp.mk
@@ -11,7 +11,7 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/twrp.fstab:$(TWRP_OUT)/etc/twrp.fstab \
-    $(COMMON_PATH)/rootdir/pulldecryptfiles.sh:$(TWRP_OUT)/sbin/pulldecryptfiles.sh
+    $(COMMON_PATH)/rootdir/sbin/pulldecryptfiles.sh:$(TWRP_OUT)/sbin/pulldecryptfiles.sh
 
 PRODUCT_COPY_FILES += \
     bionic/libc/zoneinfo/tzdata:$(TWRP_OUT)/system/usr/share/zoneinfo/tzdata
