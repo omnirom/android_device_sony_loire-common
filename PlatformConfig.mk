2a309d4cc2ba14c36ca5b006f12e659d4bd54f30
Humberto Borba <humberos@gmail.com>
2017-12-23 18:37:25 -0200

    sony: loire: Omnify
    Change-Id: Ib640bf86e41b2d79286b17f471864ed3b680122c
    Signed-off-by: Humberto Borba <humberos@gmail.com>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Platform path
-PLATFORM_COMMON_PATH := device/sony/loire
+PLATFORM_COMMON_PATH := device/sony/loire-common
 PRODUCT_PLATFORM_SOD := true
 
 TARGET_BOARD_PLATFORM := msm8952
--- platform.mk
+++ platform.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Platform path
-PLATFORM_COMMON_PATH := device/sony/loire
+PLATFORM_COMMON_PATH := device/sony/loire-common
 
 TARGET_LEGACY_KEYMASTER := true
 
